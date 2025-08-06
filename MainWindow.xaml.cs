00cd96c2c8ce5acc6136510bc9c5a59a8bafe7b0
Olga Malikova <malikovabup@mail.ru>
2025-08-07 00:10:19 +0400

    Частично добавлена логика  - простые арифметические операции

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Calculator
 {
@@ -16,6 +10,11 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string firstNumber { get; set; }
+        private string secondNumber { get; set; }
+        private string operation { get; set; }
+        private bool isCalculated { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -23,12 +22,125 @@ namespace Calculator
 
         private void NumberButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            if (isCalculated)
+            {
+                InputLabel.Content = (e.OriginalSource as Button).Content.ToString();
+                isCalculated = false;
+            }
+            else
+            {
+                InputLabel.Content += (e.OriginalSource as Button).Content.ToString();
+            }
+
         }
 
         private void SignButton_Click(object sender, RoutedEventArgs e)
         {
+            // Замена одного арифметического знака на другой
+            if (Regex.IsMatch(CalculationLabel.Content.ToString(), @"[\+\-\×/]$") && InputLabel.Content.ToString() == "")   
+            {
+                operation = (e.OriginalSource as Button).Content.ToString();
+                CalculationLabel.Content = CalculationLabel.Content.ToString().Substring(0, CalculationLabel.Content.ToString().Length - 1) + operation;
+                return;
+            }
 
+            // Если вводится знак, когда нет чисел, то первое число  - ноль
+            if (CalculationLabel.Content.ToString() == "" && InputLabel.Content.ToString() == "")
+            {
+                InputLabel.Content = "0";
+            }
+
+            // Сохраняется первое число
+            if (CalculationLabel.Content.ToString() == "")         
+            {
+                firstNumber = InputLabel.Content.ToString().TrimEnd(',');
+            }
+            // Сохраняется второе число
+            else
+            {
+
+                secondNumber = InputLabel.Content.ToString().TrimEnd(',');
+
+                if (secondNumber == "0" && operation == "/")
+                {
+                    MessageBox.Show("Ошибка! На ноль делить нельзя!");
+                }
+                else
+                {
+                    firstNumber = Calculate(decimal.Parse(firstNumber), decimal.Parse(secondNumber), operation);
+                }
+
+            }
+            operation = (e.OriginalSource as Button).Content.ToString();
+            
+            CalculationLabel.Content = firstNumber + operation;
+            InputLabel.Content = "";
         }
+        private void EqualButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (InputLabel.Content == "")
+            {
+                InputLabel.Content = Calculate(decimal.Parse(firstNumber), 0, operation);
+            }
+            else
+            {
+                secondNumber = InputLabel.Content.ToString();
+                InputLabel.Content = Calculate(decimal.Parse(firstNumber), decimal.Parse(secondNumber), operation);
+            }
+            CalculationLabel.Content = "";
+            operation = "";
+            firstNumber = "";
+            secondNumber = "";
+            isCalculated = true;
+        }
+
+        private void AlgebraicSignButton_Click(object sender, RoutedEventArgs e)
+        {
+            string number = InputLabel.Content.ToString();
+            if (number.StartsWith('-'))
+            {
+                InputLabel.Content = number.Remove(0, 1);
+            }
+            else
+            {
+                InputLabel.Content = "-" + number;
+            }
+        }
+
+        private void FloatPointButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!InputLabel.Content.ToString().Contains(','))
+            {
+                InputLabel.Content += ",";
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            CalculationLabel.Content = "";
+            InputLabel.Content = "";
+            operation = "";
+            firstNumber = "";
+            secondNumber = "";
+        }
+
+        private string Calculate(decimal firstNumber, decimal secondNumber, string sign)
+        {
+            switch (sign)
+            {
+                case "+":
+                    return (firstNumber + secondNumber).ToString();
+                case "-":
+                    return (firstNumber - secondNumber).ToString();
+                case "×":
+                    return (firstNumber * secondNumber).ToString();
+                case "/":
+                    return (firstNumber / secondNumber).ToString();
+                default:
+                    return null;
+            }
+        }
+
+
     }
 }
